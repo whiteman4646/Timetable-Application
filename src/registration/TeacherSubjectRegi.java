cb32a34487d2940c7264e1ed2184798f5234f446
hayato maeda <h.maeda1711@morijyobi.ac.jp>
2018-11-05 14:34:48 +0900

    first commit

---
+++ src/registration/TeacherSubjectRegi.java
@@ -0,0 +1,19 @@
+package registration;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TeacherSubjectRegi extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
